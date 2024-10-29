edb5d22a5ebaecca49c92d85e8b4685491419f39
PromiseUdo <info.promiseudo@gmail.com>
2024-10-29 12:39:56 +0100

    update server link

--- server/index.js
+++ server/index.js
@@ -9,11 +9,17 @@ import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
 const app = express();
 
 dotenv.config();
-const PORT = process.env.PORT || 5000;  
+const PORT = process.env.PORT || 5000;
 
 connectDB();
 
-app.use(cors());
+const corsOptions = {
+  origin: "https://musetec-login-app.vercel.app",
+  credentials: true,
+  optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
